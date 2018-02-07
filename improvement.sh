2f3e0871ecfccfe63e24617f8e15736cb67a22d6
stayupthetree <stayupthetree@gmail.com>
2018-02-07 18:13:05 -0500

    ssss

--- improvement.sh
+++ improvement.sh
@@ -4,7 +4,8 @@ echo "TH3D OctoPrint Improver"
 echo "Credit to Timothy Hoogland from https://www.th3dstudio.com/ for well...everything"
 echo "WARNING! This script will reboot your Pi on completion! DO NOT Run if you are in the middle of a print!!"
 echo -n "Do you wish to continue (y/n)?"
-read -n 1 answer
+sleep 1
+read -n 2 answer
 if echo "$answer" | grep -iq "^y" ;then
     echo Yes
     echo "Installing OctoPrint-FullScreen Plugin"
